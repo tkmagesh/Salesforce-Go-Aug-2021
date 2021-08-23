6b304ca0e509f6e70822a9ff601611e50a69d619
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-24 02:43:19 +0530

    functions intro

---
+++ 03-functions/demo-01.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(add(100, 200))
+	//fmt.Println(divide(100, 7))
+	q, _ := divide(100, 7)
+	fmt.Println(q)
+}
+
+/*
+func add(x int, y int) int {
+	return x + y
+}
+*/
+
+/*
+func add(x, y int) int {
+	return x + y
+}
+*/
+
+func add(x, y int) (result int) {
+	result = x + y
+	return
+}
+
+/* func divide(x, y int) (int, int) {
+	quotient := x / y
+	remainder := x % y
+	return quotient, remainder
+} */
+
+func divide(x, y int) (quotient int, remainder int) {
+	quotient = x / y
+	remainder = x % y
+	return
+}
