77ea90ea928788d469e78f8104a34cf62208615f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-25 22:41:09 +0530

    Update assignment.go

--- 09-interfaces/assignment.go
+++ 09-interfaces/assignment.go
@@ -6,14 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Println(sum())                                //=> 0
-	fmt.Println(sum(10))                              //=> 10
-	fmt.Println(sum(10, 20))                          //=> 30
-	fmt.Println(sum(10, 20, 30, 40))                  //=> 100
-	fmt.Println(sum(10, 20, "30", 40))                //=> 100
-	fmt.Println(sum(10, 20, "abc", 40))               //=> 70
-	fmt.Println(sum(10, 20, []int{30, 40}))           //=> 100
-	fmt.Println(sum(10, 20, []interface{}{"30", 40})) //=> 100
+	fmt.Println(sum())                                               //=> 0
+	fmt.Println(sum(10))                                             //=> 10
+	fmt.Println(sum(10, 20))                                         //=> 30
+	fmt.Println(sum(10, 20, 30, 40))                                 //=> 100
+	fmt.Println(sum(10, 20, "30", 40))                               //=> 100
+	fmt.Println(sum(10, 20, "abc", 40))                              //=> 70
+	fmt.Println(sum(10, 20, []int{30, 40}))                          //=> 100
+	fmt.Println(sum(10, 20, []interface{}{"30", 40}))                //=> 100
+	fmt.Println(sum(10, 20, []interface{}{"30", 40, []int{50, 60}})) //=> 210
 }
 
 func sum(values ...interface{}) int {
